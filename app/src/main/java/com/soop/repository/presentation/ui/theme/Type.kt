b72ffe8d4b24ce7808a409084aacefa1a7bb303f
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2025-02-06 03:24:29 +0900

    feat: 공통 theme 설정

---
+++ app/src/main/java/com/soop/repository/presentation/ui/theme/Type.kt
@@ -0,0 +1,39 @@
+package com.soop.repository.presentation.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val Typography = Typography(
+    bodySmall = TextStyle(
+        fontFamily = PretendardFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 8.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = PretendardFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = PretendardFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = PretendardFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 15.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = PretendardFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 13.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = PretendardFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 10.sp
+    )
+)
