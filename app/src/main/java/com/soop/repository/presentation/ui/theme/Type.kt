01d8cfe7c75d0d61d4d0e78330c4d20f54eb57b1
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2025-02-06 17:48:56 +0900

    feat: text style 변경

--- app/src/main/java/com/soop/repository/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/soop/repository/presentation/ui/theme/Type.kt
@@ -16,20 +16,25 @@ val Typography = Typography(
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp
     ),
-    titleLarge = TextStyle(
+    bodyLarge = TextStyle(
+        fontFamily = PretendardFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+    titleSmall = TextStyle(
         fontFamily = PretendardFontFamily,
         fontWeight = FontWeight.SemiBold,
-        fontSize = 20.sp
+        fontSize = 13.sp
     ),
     titleMedium = TextStyle(
         fontFamily = PretendardFontFamily,
         fontWeight = FontWeight.SemiBold,
         fontSize = 15.sp
     ),
-    titleSmall = TextStyle(
+    titleLarge = TextStyle(
         fontFamily = PretendardFontFamily,
         fontWeight = FontWeight.SemiBold,
-        fontSize = 13.sp
+        fontSize = 15.sp
     ),
     labelMedium = TextStyle(
         fontFamily = PretendardFontFamily,
