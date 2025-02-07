42ff2d7074304284e40d623154440398dc07c799
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2025-02-08 03:50:19 +0900

    feat: repository 통계 컴포넌트 구현

---
+++ app/src/main/java/com/soop/repository/presentation/repositorydetail/component/RepositoryStats.kt
@@ -0,0 +1,31 @@
+package com.soop.repository.presentation.repositorydetail.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun RepositoryStats(
+    modifier: Modifier = Modifier.fillMaxWidth(),
+    stars: String,
+    watchers: String,
+    forks: String
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        StatsItem(label = "Star", value = stars)
+        StatsItem(label = "Watchers", value = watchers)
+        StatsItem(label = "Forks", value = forks)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RepositoryStatsPreview() {
+    RepositoryStats(stars = "3.9k", watchers = "3.9k", forks = "3.1k")
+}
---
+++ app/src/main/java/com/soop/repository/presentation/repositorydetail/component/StatsItem.kt
@@ -0,0 +1,37 @@
+package com.soop.repository.presentation.repositorydetail.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.soop.repository.presentation.ui.theme.AppBlack
+import com.soop.repository.presentation.ui.theme.Typography
+
+@Composable
+fun StatsItem(
+    label: String, value: String
+) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            text = label,
+            style = Typography.titleLarge,
+            color = AppBlack()
+        )
+        Text(
+            modifier = Modifier.padding(top = 3.dp),
+            text = value,
+            style = Typography.bodyLarge,
+            color = AppBlack()
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun StatsItemPreview() {
+    StatsItem(label = "Star", value = "100")
+}
