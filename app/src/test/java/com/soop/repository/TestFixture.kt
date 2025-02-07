14db9a602b14fab0ea88ac69971368dce5ed5a60
채현 <wjdcogus202@naver.com>
2025-02-08 01:14:18 +0900

    feat: network 캐시 기능 구현 (#14)
    * feat: 네트워크 캐시 기능 구현
    
    * test: 네트워크 캐시 test 작성

---
+++ app/src/test/java/com/soop/repository/TestFixture.kt
@@ -0,0 +1,7 @@
+package com.soop.repository
+
+const val CACHE_SIZE_10MB = 10L * 1024 * 1024
+const val CACHE_SIZE_5KB = 5L * 1024
+const val CACHED_1_MINUTES = 60
+const val CACHED_1_SECOND = 1
+const val EXPECTED_BODY = "chaehyun & soop"
