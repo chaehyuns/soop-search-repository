e15df4bd0f544595deac7c22fd12c4fcf60c9b7b
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2025-02-08 22:41:55 +0900

    refactor: githubRepositoryImpl로 네이밍 변경

--- app/src/main/java/com/soop/repository/di/module/RepositoryModule.kt
+++ app/src/main/java/com/soop/repository/di/module/RepositoryModule.kt
@@ -14,6 +14,6 @@ abstract class RepositoryModule {
     @Binds
     @Singleton
     abstract fun bindGithubRepository(
-        imageRepositoryImpl: DefaultGithubRepository
+        githubRepositoryImpl: DefaultGithubRepository
     ): GithubRepository
 }
