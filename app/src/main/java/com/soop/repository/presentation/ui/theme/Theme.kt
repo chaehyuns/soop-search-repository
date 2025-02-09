6cd00dc0e1ff71898a22bb687951a43ed33c735c
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2025-02-09 21:37:37 +0900

    feat: 다크 모드 대응

--- app/src/main/java/com/soop/repository/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/soop/repository/presentation/ui/theme/Theme.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -67,4 +68,14 @@ fun SearchRepositoryTheme(
         typography = Typography,
         content = content
     )
+
+    Surface(
+        color = colorScheme.background
+    ) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = Typography,
+            content = content
+        )
+    }
 }
